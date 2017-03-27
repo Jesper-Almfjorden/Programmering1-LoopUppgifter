3941dc626666a5ed018ed3946469651f415d79fc
Jesper Almfjorden <almfjorden16@gmail.com>
2017-03-27 13:41:37 +0200

    Programing01: ( Assignment Loop - Part03)  l  2017-03-27 ------------------------------------------------------------------------------------- I'm done with the second and third part and is now on the 4 part. -------------------------------------------------------------------------------------

---
+++ Programmering1_LoopUppgifter/Assets/AssignmentPart02.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AssignmentPart02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        // This is the second part/step in the assignment.
+
+        // The objectiv is to create a loop that will find one element 
+        // in the list and then print it out its index.
+
+        List<int> listPart = new List<int>();
+
+        // As before we are organizing the listParts in to diffrent element.
+
+        listPart.Add(1);
+        listPart.Add(2);
+        listPart.Add(3);
+        listPart.Add(4);
+        listPart.Add(5);
+
+        // This part of the code is searching for the same value in the list.
+        // And later it is useing it to accessing the index.
+
+        int searchValue = 4;
+
+        for (int a = 0; a < listPart.Count; a++)
+        {
+            if (searchValue == listPart[a])
+            print(a);
+
+
+        }
+    }
+}
---
+++ Programmering1_LoopUppgifter/Assets/AssignmentPart03.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssignmentPart03 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // This is the thrird part/step in the assignment.
+
+        // The objectiv is to create a loop that will turn the list so the 
+        // first element will be last and the last one first.
+
+        List<int> listPart = new List<int>();
+
+        listPart.Add(10);
+        listPart.Add(20);
+        listPart.Add(30);
+        listPart.Add(40);
+        listPart.Add(50);
+
+        for (int a = listPart.Count-1; a >= 0; a--)
+
+        {
+            print(listPart[a]);
+
+
+        }
+    }
+}
